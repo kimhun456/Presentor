0f98ff6519468ca27378eacac51a9b84ef5364be
HyunJae_Kim <kimhun456@gmail.com>
2016-06-07 19:10:51 +0900

    create server and client folder

---
+++ Server/Form2.cs
@@ -0,0 +1,38 @@
+﻿using InTheHand.Net.Sockets;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Bluetooth_ServerSide
+{
+    public partial class Form2 : Form
+    {
+        BluetoothDeviceInfo[] devices;
+        public int a;
+
+        public Form2(BluetoothDeviceInfo[] devices)
+        {
+            InitializeComponent();
+
+            this.devices = devices;
+
+            foreach (BluetoothDeviceInfo item in devices)
+            {
+                listView1.Items.Add(new ListViewItem(item.DeviceName));
+            }
+            listView1.MultiSelect = false;
+            listView1.FullRowSelect = true;
+        }
+        
+        private void listView1_ItemSelectionChanged(object sender, ListViewItemSelectionChangedEventArgs e)
+        {
+            a = listView1.FocusedItem.Index;
+            this.Close();
+        }
+    }
+}
